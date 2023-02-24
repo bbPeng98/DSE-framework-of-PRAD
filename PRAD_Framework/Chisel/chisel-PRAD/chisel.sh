93ae6440ba94deff867ba11731c51b5d64d55460
bbPeng98 <bbpeng21@m.fudan.edu.cn>
2023-02-24 17:30:54 +0800

    refresh1

--- PRAD_Framework/Chisel/chisel-PRAD/chisel.sh
+++
@@ -1,6 +0,0 @@
-
-sbt "runMain AHA_Interconnect.Instance.garnet -td ./test_run_dir --no-check-comb-loops" -mem 8192
-
-cd ./src/main/resources
-echo begin dealing with graph file: 
-python3 replace.py
